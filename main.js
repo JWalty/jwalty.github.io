a1c16ddfc91d13ade0bdc5ba70aab5e01981d15e
JWalty <jake.walterhouse@gmail.com>
2017-12-08 06:57:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 var rockPoints = 0;
 
-var rockCheater = 0;
-
-var treeCheater = 0;
-
 var robotRock = 0;
 
 var branches = 0;
@@ -12,13 +8,11 @@ var robotRockCost = 200;
 
 function increase(number){
     rockPoints = rockPoints + number;
-	rockCheater = rockCheater + 1;
     document.getElementById("rockPoints").innerHTML = rockPoints;
 };
 
 function shakeTree() {
 	branches = branches + 1;
-	treeCheater = treeCheater + 1;
     document.getElementById("branches").innerHTML = branches;
 }
 
@@ -26,6 +20,8 @@ function buyRobotRock(){
 	if (branches > robotRockCost) {
 		branches = branches - robotRockCost;
 		robotRock = robotRock + 1;
+		robotRockCost = Math.floor(200* Math.pow(1.1,robotRock));
+		document.getElementById("robotRockCost").innerHTML = robotRockCost;			
 		document.getElementById("robotRock").innerHTML = robotRock;	
 		document.getElementById("branches").innerHTML = branches;	
 }		
@@ -37,30 +33,6 @@ function autoRock(){
 }	
 	
 	
-function treeAntiCheat(){
-	if (treeCheater > 15) {
-		branches = -999999999999999;
-		treeCheater = 0;
-		document.getElementById("branches").innerHTML = branches;		
-	}
-	else {
-		treeCheater = 0;
-	}
-}
-
-function rockAntiCheat(){
-	if (rockCheater > 15) {
-		rockPoints = -999999999999999;
-		rockCheater = 0;
-		document.getElementById("rockPoints").innerHTML = rockPoints;
-	}
-	else {
-		rockCheater = 0;
-	}
-}
-		
 window.setInterval(function(){
-	rockAntiCheat();
-	treeAntiCheat();
 	autoRock();
 }, 1000);
