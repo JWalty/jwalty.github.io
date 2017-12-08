6c721602b22378040da807c336731e2b3e42d88f
JWalty <jake.walterhouse@gmail.com>
2017-12-08 06:08:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var rockPoints = 0;
+
+var cheater = 0;
+
+function increase(number){
+    rockPoints = rockPoints + number;
+	cheater = cheater + 1;
+    document.getElementById("rockPoints").innerHTML = rockPoints;
+};
+
+function antiCheat(){
+	if (cheater > 10) {
+		rockPoints = -999999999999999;
+		cheater = 0;
+	}
+	else {
+		cheater = 0;
+	}
+}
+		
+window.setInterval(function(){
+	antiCheat()
+}, 1000);
