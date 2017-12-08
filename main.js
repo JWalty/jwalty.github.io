1738808b8f8d4ba1997d7c1be9e5c95fbe40e914
JWalty <jake.walterhouse@gmail.com>
2017-12-08 06:27:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,38 @@ var rockPoints = 0;
 
 var cheater = 0;
 
+var robotRock = 0;
+
+var branches = 0;
+
+var robotRockCost = 200;
+
 function increase(number){
     rockPoints = rockPoints + number;
 	cheater = cheater + 1;
     document.getElementById("rockPoints").innerHTML = rockPoints;
 };
 
+function shakeTree() {
+	branches = branches + 1;
+    document.getElementById("branches").innerHTML = branches;
+}
+
+function buyRobotRock(){
+	if (branches > robotRockCost) {
+		branches = branches - robotRockCost;
+		robotRock = robotRock + 1;
+		document.getElementById("robotRock").innerHTML = robotRock;	
+		document.getElementById("branches").innerHTML = branches;	
+}		
+}
+
+function autoRock(){
+	rockPoints = rockPoints + robotRock;
+    document.getElementById("rockPoints").innerHTML = rockPoints;
+}	
+	
+	
 function antiCheat(){
 	if (cheater > 15) {
 		rockPoints = -999999999999999;
@@ -19,5 +45,6 @@ function antiCheat(){
 }
 		
 window.setInterval(function(){
-	antiCheat()
+	antiCheat();
+	autoRock();
 }, 1000);
