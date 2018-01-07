2c199966a3f132ef3f3d558374077899e76d4545
JWalty <jake.walterhouse@gmail.com>
2018-01-07 10:53:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ var red = 0;
 var green = 0;
 var orange = 0;
 var purple = 0;
+var blue = 0;
+
+function increaseBlue(){
+	blue = blue + 1;
+	document.getElementById("blue").innerHTML = blue;
+}	
 
 function increasePurple(){
 	purple = purple + 1;
@@ -11,29 +17,28 @@ function increasePurple(){
 function increaseOrange(){
 	orange = orange + 1;
 	document.getElementById("orange").innerHTML = orange;
-	unlockPurple();	
+	unlock();	
 }	
 
 
 function increaseGreen(){
 	green = green + 1;
 	document.getElementById("green").innerHTML = green;
-	unlockPurple();	
+	unlock();	
 }	
 
 function increaseRed(){
 	red = red + 1;
 	document.getElementById("red").innerHTML = red;
-	unlockPurple();
+	unlock();
 }
 
-function unlockPurple(){
-        if (red === 10) {
-			if (orange === 10) {
-				if (green === 10) {
-					myDIV.style.display = "block";
-				}
-			}
+function unlock(){
+        if (red === 10 && green === 10 && orange === 10) {
+			purpleButton.style.display = "block";
+		}
+        if (red === 1 && green === 1 && orange === 1) {
+			blueButton.style.display = "block";
 		}
 }
 
