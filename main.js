7223deb2d0a48233c32bcbc695aad391ba179632
JWalty <jake.walterhouse@gmail.com>
2018-01-07 10:32:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 var red = 0;
 var green = 0;
+var orange = 0;
+var purple = 0;
+
+function increasePurple(){
+	purple = purple + 1;
+	document.getElementById("purple").innerHTML = purple;
+}	
+
+function increaseOrange(){
+	orange = orange + 1;
+	document.getElementById("orange").innerHTML = orange;
+	unlockPurple();	
+}	
+
 
 function increaseGreen(){
 	green = green + 1;
 	document.getElementById("green").innerHTML = green;
+	unlockPurple();	
 }	
 
 function increaseRed(){
 	red = red + 1;
 	document.getElementById("red").innerHTML = red;
-}	
+	unlockPurple();
+}
 
+function unlockPurple(){
+        if (red === 10) {
+			if (orange === 10) {
+				if (green === 10) {
+					myDIV.style.display = "block";
+				}
+			}
+		}
+}
 
 
 window.setInterval(function(){
