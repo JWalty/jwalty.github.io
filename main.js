f7ca89c4f089b5c801f0620799970c5db8f7ede0
JWalty <jake.walterhouse@gmail.com>
2020-04-18 21:50:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,11 @@ var i = 0;
 var character1;
 var possibleCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-
-
 function cycleCharacter1(){
-	character1 = possibleCharacters[i];
-	i = i + 1;
-	if (possibleCharacters[i] == "Z") {
-		i = 0;
-	};
+	character1 = possibleCharacters[Math.round(Math.random() * 25)];
     document.getElementById("character1").innerHTML = character1;	
 }
 
-
 window.setInterval(function(){
-
 	cycleCharacter1()
 }, 200);
